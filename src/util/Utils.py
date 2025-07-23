8cb9e87c8fa83f5771caf153a5fb0279f29f2849
alexp8 <amp1993@gmail.com>
2025-07-23 12:16:42 -0400

    housekeeping

--- src/util/Utils.py
+++ src/util/Utils.py
@@ -5,6 +5,10 @@ from src.constants.Constants import *
 from io import StringIO
 import discord
 
+from src.util.MyLogger import MyLogger
+
+logger = MyLogger("utils")
+
 
 def is_error(obj):
     return isinstance(obj, Exception) or isinstance(obj, APIRequestError)
@@ -12,14 +16,17 @@ def is_error(obj):
 
 # Send error message to channel on discord for bot logs
 async def send_message_to_log_channel(bot: Bot, msg: str):
-    channel = bot.get_channel(CNCNET_LADDER_DEV_DISCORD_BOT_LOGS_ID)
+    try:
+        channel = bot.get_channel(CNCNET_LADDER_DEV_DISCORD_BOT_LOGS_ID)
 
-    if len(msg) > 3000:
-        buffer = StringIO(msg)
-        f = discord.File(buffer, filename=f"error.txt")
-        await channel.send(f"Error", file=f)
-    else:
-        await channel.send(msg[:3000])
+        if len(msg) > 3000:
+            buffer = StringIO(msg)
+            f = discord.File(buffer, filename="error.txt")
+            await channel.send("Error", file=f)
+        else:
+            await channel.send(msg[:3000])
+    except discord.errors.DiscordServerError as e:
+        logger.exception("Failed to send message to log channel")
 
 
 def get_exception_msg(e):
